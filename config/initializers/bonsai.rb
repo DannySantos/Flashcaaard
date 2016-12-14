6ab24825bdcc0db5a8c488d42e277cec8283f948
Danny Santos <danny@ethicalswitch.com>
2016-12-14 14:34:26 +0000

    Init agen

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,1 @@
-#if ENV['BONSAI_URL']
-#  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
-#end
+Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
