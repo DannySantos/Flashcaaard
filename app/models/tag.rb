d6098245a5bc7ca372da277ee2afdd43040775e7
Danny Santos <danny@ethicalswitch.com>
2016-12-14 14:16:51 +0000

    Started Bonsai

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,10 @@
 class Tag < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
   searchkick autocomplete: ['name']
 
   has_many :flashcard_tags
   has_many :flashcards, through: :flashcard_tags
 end
+
+Tag.import
